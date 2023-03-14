2db2d2fbae1d014e36bf799d98319b4319d34dac
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-03-13 22:06:00 -0300

    zod validation and react-hook-form

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -19,25 +19,33 @@ export function History() {
               <td>Task 1 </td>
               <td>20 min </td>
               <td>Two months ago </td>
-              <td>Concluded </td>
+              <td>
+                <Status statusColor="green">Concluded</Status>
+              </td>
             </tr>
             <tr>
               <td>Task 1 </td>
               <td>20 min </td>
               <td>Two months ago </td>
-              <td>Concluded </td>
+              <td>
+                <Status statusColor="red">Concluded</Status>
+              </td>
             </tr>
             <tr>
               <td>Task 1 </td>
               <td>20 min </td>
               <td>Two months ago </td>
-              <td>Concluded </td>
+              <td>
+                <Status statusColor="yellow">Concluded</Status>
+              </td>
             </tr>
             <tr>
               <td>Task 1 </td>
               <td>20 min </td>
               <td>Two months ago </td>
-              <td>Concluded </td>
+              <td>
+                <Status statusColor="green">Concluded</Status>
+              </td>
             </tr>
           </tbody>
         </table>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,8 @@
 import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -9,16 +13,49 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(3, 'Write the task'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'Cycle must be at lest 5 minutes')
+    .max(60, 'interval must be max of 60 minutes'),
+})
+
+// type - criar tipagem a partir de outra variavel ou referencia - interface: definir o objeto
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitDisabled = !task
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">I will work in</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Give a name to your project"
+            {...register('task')}
           />
 
           <datalist id="task-suggestions">
@@ -35,7 +72,7 @@ export function Home() {
             placeholder="00"
             step={5}
             min={5}
-            max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -49,7 +86,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Start
         </StartCountdownButton>
