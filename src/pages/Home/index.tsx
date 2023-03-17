2ebed2307e0e95b6d44e5790ada3d6cda18885fa
Leticia Gonçalves <leticiagoncalves.tech@gmail.com>
2023-03-16 23:34:34 -0300

    fixing reset countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -34,14 +34,19 @@ export function Home() {
     },
   })
 
-  const { handleSubmit, watch /* reset */ } = newCycleForm
+  const { handleSubmit, watch, reset } = newCycleForm
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    createNewCycle(data)
+    reset()
+  }
 
   const task = watch('task')
   const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form onSubmit={handleSubmit(createNewCycle)}>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormProvider {...newCycleForm}>
           <NewCycleForm />
         </FormProvider>
